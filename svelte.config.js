6b3f957a495c03a3d67ccba76be996c4ae505569
adrian-cy <adrian@crowdy.ca>
2022-04-22 19:56:40 -0400

    make sure site is static built

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,18 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
 
